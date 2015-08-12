f8fdc0d430ee56ea1ad74a2e89977c83807ef5cc
dshulgin2015 <unetby2016@yandex.ru>
2015-08-12 16:49:52 +0300

    Found out a solution of MFR problem

--- Mortal_Fibb_Rabbits.py
+++ Mortal_Fibb_Rabbits.py
@@ -1,20 +1,16 @@
-#run for n months, rabbits die after m months.
-n, m = input("Enter months to run, and how many months rabbits live, separated by a space").split()
-n, m = int(n), int(m)
-generations = [1, 1] #Seed the sequence with the 1 pair, then in their reproductive month.
-def fib(i, j):
-    count = 2
-    while (count < i):
-        if (count < j):
-            generations.append(generations[-2] + generations[-1]) #recurrence relation before rabbits start dying (simply fib seq Fn = Fn-2 + Fn-1)
-        elif (count == j or count == j+1):
-            print ("in base cases for newborns (1st+2nd gen. deaths)") #Base cases for subtracting rabbit deaths (1 death in first 2 death gens)
-            generations.append((generations[-2] + generations[-1]) - 1)#Fn = Fn-2 + Fn-1 - 1
-        else:
-            generations.append((generations[-2] + generations[-1]) - (generations[-(j+1)])) #Our recurrence relation here is Fn-2 + Fn-1 - Fn-(j+1)
-        count += 1
-    return (generations[-1])
+n, k = (int (x) for x in raw_input().split())
+
+f = [0] * (n + 1)
+f[0] = 1
+for i in range (2, n + 1):
+    f[i] = f[i-2] + f[i-1] - f[i - k - 1]
+
+print f[n] + f[n-1]
 
 
-print (fib(n, m))
-print ("Here's how the total population looks by generation: \n" + str(generations))
+#!/usr/bin/env python
+def fib(n,k=1):
+  ages = [1] + [0]*(k-1)
+  for i in xrange(n-1):
+    ages = [sum(ages[1:])] + ages[:-1]
+  return sum(ages)
